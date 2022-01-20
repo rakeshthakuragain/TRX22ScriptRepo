814c463c8b30ac89b3a76564221867ba35317896
Thakur <zkx3un@splm.siemens.com>
2022-01-20 18:15:55 +0530

    a

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "Here in build.sh script on host "
+echo "Release = $1"
+echo "Path = $2"
+/usr/site/devop_tools/bin/unit add -b -p @${NXRelease} -t DEV -w SUB ${UnitPath}/${NXRelease}_TranslatorWorker_1 -R y -O y -DO_LINK_OPT y
