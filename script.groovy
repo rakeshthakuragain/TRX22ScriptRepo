11de6bdaebb4923c4220741958253cfa0c04d272
Thakur <zkx3un@splm.siemens.com>
2022-01-21 11:19:56 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildUnit()
 	script{
 		echo "Building here on machine with ${params.NXRelease} and ${params.UnitPath}"
 		
-		def unitFullPath=${params.UnitPath}/${params.NXRelease}_TranslatorWorker_${BUILD_TIMESTAMP}
+		def unitFullPath="${params.UnitPath}/${params.NXRelease}_TranslatorWorker_${BUILD_TIMESTAMP}"
 		sh "chmod +x ./build.sh "
 		sh "./build.sh ${params.NXRelease} ${unitFullPath}"
 		
